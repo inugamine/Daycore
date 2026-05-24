388bc46333244c601c7093f49534b7cf95c3a9b7
inugamine <zh20d0m0iztxkg5f5914@gmail.com>
2026-05-25 02:28:41 +0900

    最初のコミット

---
+++ Daycore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daycore
+//
+//  Created by Shouta on 2026/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daycore/DaycoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaycoreApp.swift
+//  Daycore
+//
+//  Created by Shouta on 2026/05/25.
+//
+
+import SwiftUI
+
+@main
+struct DaycoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
